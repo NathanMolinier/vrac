7fb442360cc0fef825a32e1fc71376737d0a977b
Nathan Molinier <nathan.molinier@polymtl.ca>
2023-12-04 18:54:21 -0500

    update with new BIDS standard

--- BIDSIFICATION/convert_to_BIDS_KL.py
+++ BIDSIFICATION/convert_to_BIDS_KL.py
@@ -18,8 +18,8 @@ logging.root.addHandler(hdlr)
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Convert dataset to BIDS format.')
-    parser.add_argument("-i", "--path-input",
-                        help="Path to the folder containing non-BIDS dataset.",
+    parser.add_argument("-i", "--path-dataset",
+                        help="Path to the non-BIDS dataset.",
                         required=True)
     parser.add_argument("-o", "--path-output",
                         help="Path to the output folder where the BIDS dataset will be stored.",
@@ -71,14 +71,28 @@ def gzip_nii(path_nii):
         os.system(f'gzip -f {path_nii}')
 
 
-def create_empty_json_file(path_file_out):
+def create_json_file(path_file_out):
     """
-    Create an empty json sidecar file
+    Create a json sidecar file
     :param path_file_out: path to the output file
     """
     path_json_out = path_file_out.replace('.nii.gz', '.json')
+    data_json = {
+        "SpatialReference": "orig",
+        "GeneratedBy": [
+            {
+                "Name": "Manual",
+                "Description": "Manually created by our collaborators"
+            },
+            {
+                "Name": "Quality Control",
+                "Author": "Nathan Molinier",
+                "Date": time.strftime('%Y-%m-%d %H:%M:%S')
+            }
+        ]
+    }
     with open(path_json_out, 'w') as f:
-        json.dump({}, f)
+        json.dump(data_json, f, indent=4)
         logger.info(f'Created: {path_json_out}')
 
 
@@ -139,11 +153,18 @@ def create_dataset_description(path_output):
     :return:
     """
     data_json = {
-        "BIDSVersion": "BIDS 1.8.0",
+        "BIDSVersion": "BIDS 1.9.0",
         "Name": "lumbar-vanderbilt",
         "DatasetType": "derivative",
-        "Authors": [
-            "Nathan Molinier"
+        "GeneratedBy": [
+            {
+                "Name": "Manual",
+                "Description": "Manually created by our collaborators"
+            },
+            {
+                "Name": "Quality Control",
+                "Description": "Manually controlled by Nathan Molinier"
+            }
         ]
     }
     write_json(path_output, 'dataset_description.json', data_json)
@@ -156,7 +177,7 @@ def create_dataset_description_sourcedata(path_output):
     :return:
     """
     data_json = {
-        "BIDSVersion": "BIDS 1.8.0",
+        "BIDSVersion": "BIDS 1.9.0",
         "Name": "lumbar-vanderbilt",
         "DatasetType": "raw"
     }
@@ -186,14 +207,14 @@ def main():
     args = parser.parse_args()
 
     # Make sure that input args are absolute paths
-    path_input = os.path.abspath(args.path_input)
+    path_dataset = os.path.abspath(args.path_dataset)
     path_output = os.path.abspath(args.path_output)
 
     # Check if input path is valid
-    if not os.path.isdir(path_input):
-        print(f'ERROR - {path_input} does not exist.')
+    if not os.path.isdir(path_dataset):
+        print(f'ERROR - {path_dataset} does not exist.')
         sys.exit()
-        
+    
     # Create output folder if it does not exist
     if not os.path.isdir(path_output):
         os.makedirs(path_output, exist_ok=True)
@@ -202,6 +223,7 @@ def main():
     # Dump log file there
     if os.path.exists(FNAME_LOG):
         os.remove(FNAME_LOG)
+    
     fh = logging.FileHandler(os.path.join(os.path.abspath(os.curdir), FNAME_LOG))
     logging.root.addHandler(fh)
     logger.info("INFO: log file will be saved to {}".format(FNAME_LOG))
@@ -218,71 +240,139 @@ def main():
     else:
         sub_dict_tsv = dict()
     
-    sub_list = sorted([d for d in os.listdir(path_input) if not d.startswith('.')])
-    derivative_path = 'derivatives/labels/'
-    for sub in sub_list:
-        for file in os.listdir(os.path.join(path_input, sub)):
-            if file.endswith('.nii.gz'):                    
-                # Create new names bids
-                # Denomination are based on sct_testing_large
-                subject_name_bids = 'sub-' + sub
+    for dir in os.listdir(path_dataset):
+        path_input = os.path.join(path_dataset, dir)
+        if os.path.isdir(path_input):
+            sub_list = sorted([d for d in os.listdir(path_input) if not d.startswith('.')])
+            derivative_path = 'derivatives/clinical-labels/'
+            for sub in sub_list:
+                # Create sub_dict for QC control
+                sub_dict = dict()
+                for file in os.listdir(os.path.join(path_input, sub)):
+                    if file.endswith('.nii.gz'):                    
+                        # Create new names bids
+                        # Denomination are based on sct_testing_large
+                        subject_name_bids = 'sub-' + sub
 
-                # Path input image
-                path_file_in = os.path.join(path_input, sub, file)
+                        # Add subject name to participant.tsv 
+                        if subject_name_bids not in sub_dict_tsv.keys(): # Add only one time each subject into the participant.csv
+                            # Aggregate subjects for participants.tsv
+                            sub_dict_tsv[subject_name_bids] = sub
 
-                # Fetch image acquisition type
-                if 'Axial' in path_input:
-                    acq = '_acq-axial'
-                elif 'Sagittal' in path_input:
-                    acq = '_acq-sagittal'
+                        # Path input image
+                        path_file_in = os.path.join(path_input, sub, file)
 
-                # Fetch contrast
-                if 'mffe' in file:
-                    cont = '_T2star'
-                elif 'Sagittal' in path_input:
-                    cont = '_T2w'
+                        # Fetch image acquisition type
+                        if 'Axial' in path_input:
+                            acq_entity = 'acq-axial'
+                        elif 'Sagittal' in path_input:
+                            acq_entity = 'acq-sagittal'
 
-                # Fetch image type
-                if '_disc_labels' in file:
-                    img_type = 'MASK'
-                    suffix = '_labels-disc'
-                if '_gmseg' in file:
-                    img_type = 'MASK'
-                    suffix = '_label-GM_seg'
-                if '_seg' in file:
-                    img_type = 'MASK'
-                    suffix = '_label-SC_seg'
-                else:
-                    img_type = 'IMAGE'
+                        # Fetch contrast
+                        if 'mffe' in file:
+                            cont = 'T2star'
+                        elif 'Sagittal' in path_input:
+                            cont = 'T2w'
 
-                if img_type == 'IMAGE':
-                    # Construct path for the output IMAGE
-                    path_subject_folder_out = os.path.join(path_output, subject_name_bids, 'anat')
-                    create_subject_folder_if_not_exists(path_subject_folder_out)
-                    path_file_out = os.path.join(path_subject_folder_out, subject_name_bids + acq + cont + '.nii.gz')
-                
-                    # Copy nii file to the output dataset
-                    copy_nii(path_file_in, path_file_out)
-                elif img_type == 'MASK':
-                    # Construct path for the output MASK
-                    path_subject_folder_out = os.path.join(path_output, derivative_path, subject_name_bids, 'anat')
-                    create_subject_folder_if_not_exists(path_subject_folder_out)
-                    path_file_out = os.path.join(path_subject_folder_out, subject_name_bids + acq + cont + suffix + '.nii.gz')
-                
-                    # Copy nii file to the output dataset
-                    copy_nii(path_file_in, path_file_out)
-                    
-                # Copy or create an empty json sidecar file
-                path_in_json = path_file_in.replace('.nii.gz', '.json')
-                path_out_json = path_file_out.replace('.nii.gz', '.json')
-                if os.path.exists(path_in_json):
-                    copy(path_in_json, path_out_json)
-                else:
-                    create_empty_json_file(path_file_out)
-                
-                if subject_name_bids not in sub_dict_tsv.keys(): # Add only one time each subject into the participant.csv
-                    # Aggregate subjects for participants.tsv
-                    sub_dict_tsv[subject_name_bids] = sub
+                        # Fetch image type
+                        if '_disc_labels' in file:
+                            img_type = 'MASK'
+                            suffix = 'dlabel'
+                            desc_entity = f'desc-{cont}'
+                            comp_entity = 'seg-discs'
+                        elif '_gmseg' in file:
+                            img_type = 'MASK'
+                            suffix = 'mask'
+                            desc_entity = f'desc-{cont}'
+                            comp_entity = 'label-GM'
+                        elif '_seg' in file:
+                            img_type = 'MASK'
+                            suffix = 'mask'
+                            desc_entity = f'desc-{cont}'
+                            comp_entity = 'label-SC'
+                        else:
+                            img_type = 'IMAGE'
+                            suffix = cont
+
+                        if img_type == 'IMAGE':
+                            # Construct path for the output IMAGE
+                            path_subject_folder_out = os.path.join(path_output, subject_name_bids, 'anat')
+                            create_subject_folder_if_not_exists(path_subject_folder_out)
+                            filename_out = "_".join([subject_name_bids, acq_entity, suffix + '.nii.gz'])
+                            path_file_out = os.path.join(path_subject_folder_out, filename_out)
+
+                            # Add paths to dict for QC control
+                            if 'IMAGE' in sub_dict.keys():
+                                raise ValueError('Mutiple images were detected')
+                            sub_dict['IMAGE'] = {'path_file_in':path_file_in, 'path_file_out':path_file_out}
+
+                            # Copy nii
+                            copy_nii(path_file_in, path_file_out)
+
+                        elif img_type == 'MASK':
+                            # Construct path for the output MASK
+                            path_subject_folder_out = os.path.join(path_output, derivative_path, subject_name_bids, 'anat')
+                            create_subject_folder_if_not_exists(path_subject_folder_out)
+                            filename_out = "_".join([subject_name_bids, acq_entity, comp_entity, desc_entity, suffix + '.nii.gz'])
+                            path_file_out = os.path.join(path_subject_folder_out, filename_out)
+
+                            # Add paths to dict for QC control
+                            if f'MASK_{comp_entity}' in sub_dict.keys():
+                                raise ValueError(f'Mutiple masks {comp_entity} were detected')
+                            sub_dict[f'MASK_{comp_entity}'] = {'path_file_in':path_file_in, 'path_file_out':path_file_out}
+
+                for img_type, dict_paths in sub_dict.items():
+                    if img_type.startswith('MASK'):
+                        # Load raw image
+                        img_path = sub_dict['IMAGE']['path_file_in']
+                        img = Image(img_path)
+                        orientation_img = img.orientation
+
+                        # Load mask data
+                        path_mask_in = dict_paths['path_file_in']
+                        path_mask_out = dict_paths['path_file_out']
+                        mask = Image(path_mask_in)
+                        orientation_mask = mask.orientation
+
+                        # Check orientation
+                        if not orientation_img == orientation_mask:
+                            logger.info(f"{img_path} and {path_mask_in} have different orientations, changing to image orientation {orientation_img}")
+                            mask.change_orientation(orientation_img)
+
+                        # Check image dimensions
+                        if not img.data.shape == mask.data.shape:
+                            raise ValueError(f"{img_path} and {path_mask_in} have different dimensions !")
+
+                        # Add files to QC
+                        if img_type.split('_')[1] == 'label-SC':
+                            # Add corresponding sct function
+                            corr_sct_function = 'sct_deepseg_sc'
+                        elif img_type.split('_')[1] == 'label-GM':
+                            # Add corresponding sct function
+                            corr_sct_function = 'sct_deepseg_gm'
+                        elif img_type.split('_')[1] == 'seg-discs':
+                            # Add corresponding sct function
+                            corr_sct_function = 'sct_label_utils'
+                        else:
+                            raise ValueError(f"Unexpected image type: {img_type.split('_')[1]}")
+
+                        # Add image to QC report
+                        subprocess.check_call(['sct_qc',
+                                                '-i', img_path,
+                                                '-s', path_mask_in,
+                                                '-p', corr_sct_function,
+                                                '-qc', os.path.join(path_output, 'qc')])
+
+                        # Copy or initialize a json sidecar file
+                        path_in_json = path_mask_in.replace('.nii.gz', '.json')
+                        path_out_json = path_mask_out.replace('.nii.gz', '.json')
+                        if os.path.exists(path_in_json):
+                            copy(path_in_json, path_out_json)
+                        else:
+                            create_json_file(path_out_json)
+
+                        # Copy mask to output folder
+                        copy_nii(path_mask_in, path_mask_out)
     
     participants_tsv_list = list(sub_dict_tsv.items())                 
 
