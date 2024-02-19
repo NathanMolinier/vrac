c0c0816036d298aec41729f8fa02a53170699a45
Nathan Molinier <nathan.molinier@polymtl.ca>
2024-02-19 11:24:09 -0500

    update code to work on single images

--- keep_largest_component.py
+++ keep_largest_component.py
@@ -1,135 +1,67 @@
 """
-Copied from https://github.com/neuropoly/totalsegmentator-mri 
+Based on https://github.com/neuropoly/totalsegmentator-mri 
 """
 
-import sys, argparse, textwrap
+import os, argparse, textwrap
 from scipy.ndimage import label
 from pathlib import Path
 import numpy as np
 import nibabel as nib
-import multiprocessing as mp
-from functools import partial
-from tqdm.contrib.concurrent import process_map
 
 
-def main():
-    
-    # Parse command line arguments
+def get_parser():
+    # parse command line arguments
     parser = argparse.ArgumentParser(
         description=textwrap.dedent(f'''
-            This script processes NIfTI segmentation files, leaving the largest component for each label.
-        '''),
-        epilog=textwrap.dedent('''
-            Examples:
-            generate_largest_labels -s labels_init -o labels
+            This script processes a NIfTI segmentation file, leaving the largest component for each label.
         '''),
         formatter_class=argparse.RawTextHelpFormatter
     )
-
     parser.add_argument(
-        '--seg-dir', '-s', type=DirPath(), required=True,
-        help='Folder containing input segmentations.'
+        '--seg-in', '-s', type=str, required=True,
+        help='Input segmentation path'
     )
     parser.add_argument(
-        '--output-dir', '-o', type=DirPath(True), required=True, 
-        help='Folder to save output segmentations.'
-    )
-    parser.add_argument(
-        '--subject-dir', '-d', type=str, default=None, nargs='?', const='',
-        help=textwrap.dedent('''
-            Is every subject has its oen direcrory.
-            If this argument will be provided without value it will look for any directory in the segmentation directory.
-            If value also provided it will be used as a prefix to subject directory (for example "sub-"), defaults to False (no subjet directory).
-        '''),
-    )
-    parser.add_argument(
-        '--subject-subdir', '-u', type=str, default='', 
-        help='Subfolder inside subject folder containing masks (for example "anat"), defaults to no subfolder.'
-    )
-    parser.add_argument(
-        '--prefix', '-p', type=str, default='', 
-        help='File prefix to work on.'
-    )
-    parser.add_argument(
-        '--seg-suffix', type=str, default='',
-        help='Segmentation suffix, defaults to "".'
-    )
-    parser.add_argument(
-        '--output-seg-suffix', type=str, default='',
-        help='Suffix for output segmentation, defaults to "".'
-    )
-    parser.add_argument(
-        '--max-workers', '-w', type=int, default=min(32, mp.cpu_count() + 4),
-        help='Max worker to run in parallel proccess, defaults to min(32, mp.cpu_count() + 4).'
+        '--seg-out', type=str, required=True, 
+        help='Output segmentation path'
     )
     parser.add_argument(
         '--verbose', '-v', type=int, default=1, choices=[0, 1],
         help='Verbosity level. 0: Errors/warnings only, 1: Errors/warnings + info (default: 1)'
     )
+    return parser
 
-    try:
-        args = parser.parse_args()
-    except BaseException as e:
-        sys.exit()
+
+def main():
+    """
+    """
+    parser = get_parser()
+    args = parser.parse_args()
 
     # Get arguments
-    seg_path = args.seg_dir
-    output_path = args.output_dir
-    subject_dir = args.subject_dir
-    subject_subdir = args.subject_subdir
-    prefix = args.prefix
-    seg_suffix = args.seg_suffix
-    output_seg_suffix = args.output_seg_suffix
-    max_workers = args.max_workers
+    seg_in = args.seg_in
+    seg_out = args.seg_out
     verbose = args.verbose
 
     if verbose:
         print(textwrap.dedent(f'''
             Running {Path(__file__).stem} with the following params:
-            seg_dir = "{seg_path}"
-            output_dir = "{output_path}"
-            subject_dir = "{subject_dir}"
-            subject_subdir = "{subject_subdir}"
-            prefix = "{prefix}"
-            seg_suffix = "{seg_suffix}"
-            output_seg_suffix = "{output_seg_suffix}"
-            max_workers = "{max_workers}"
+            seg_in = "{seg_in}"
+            seg_out = "{seg_out}"
             verbose = {verbose}
         '''))
     
-    glob_pattern = ""
-    if subject_dir is not None:
-        glob_pattern += f"{subject_dir}*/"
-    if len(subject_subdir) > 0:
-        glob_pattern += f"{subject_subdir}/"
-    glob_pattern += f'{prefix}*{seg_suffix}.nii.gz'
-
-    # Process the NIfTI image and segmentation files
-    segs_path_list = list(seg_path.glob(glob_pattern))
-
-    # Create a partially-applied function with the extra arguments
-    partial_generate_largest_labels = partial(
-        generate_largest_labels,
-        output_path=output_path,
-        seg_suffix=seg_suffix,
-        output_seg_suffix=output_seg_suffix,
-   )
-
-    with mp.Pool() as pool:
-        process_map(partial_generate_largest_labels, segs_path_list, max_workers=max_workers)
+    # Keep largest component
+    keep_largest_component(seg_in, seg_out)
     
 
-def generate_largest_labels(
-            seg_path,
-            output_path,
-            seg_suffix,
-            output_seg_suffix,
+def keep_largest_component(
+            seg_in,
+            seg_out
         ):
     
-    output_seg_path = output_path / seg_path.name.replace(f'{seg_suffix}.nii.gz', f'{output_seg_suffix}.nii.gz')
-
     # Load segmentation
-    seg = nib.load(seg_path)
+    seg = nib.load(seg_in)
     seg_data = seg.get_fdata()
 
     # Convert data to uint8 to avoid issues with segmentation IDs
@@ -148,38 +80,14 @@ def generate_largest_labels(
     # Create result segmentation
     seg = nib.Nifti1Image(seg_data, seg.affine, seg.header)
     seg.set_data_dtype(np.uint8)
+
     # Make sure output directory exists
-    output_seg_path.parent.mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(os.path.dirname(seg_out)):
+        os.makedirs(os.path.dirname(seg_out))
+
     # Save mapped segmentation
-    nib.save(seg, output_seg_path)
+    nib.save(seg, seg_out)
 
-
-class DirPath:
-    """
-    Get path from argparse and return as Path object.
-    
-    Args:
-        create: Create directory if it doesn't exist
-        
-    """
-
-    def __init__(self, create=False):
-        self.create = create
-
-    def __call__(self, dir_path):
-        path = Path(dir_path)
-        
-        if self.create and not path.exists():
-            try:
-                path.mkdir(parents=True, exist_ok=True) 
-            except:
-                pass
-                
-        if path.is_dir():
-            return path
-        else:
-            raise argparse.ArgumentTypeError(
-                f"readble_dir:{path} is not a valid path")
 
 if __name__ == '__main__':
     main()
