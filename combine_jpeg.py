234c2bb14138687b1c5488db8b5c096f0e4fbc3c
Nathan Molinier <nathan.molinier@gmail.com>
2024-07-18 10:56:42 -0400

    add script to combine jpeg

---
+++ combine_jpeg.py
@@ -0,0 +1,47 @@
+import os
+from PIL import Image
+import cv2
+import numpy as np
+
+jpeg_folder = '/Users/nathan/Desktop/preview_totalseg'
+sub_string = 'step1'
+
+nb_column = 6
+col_list = []
+line_list = []
+img_name = []
+jpeg_list = os.listdir(jpeg_folder)
+
+# Get max image shape for padding
+shape = []
+for i, jpeg in enumerate(jpeg_list):
+    if '.jpg' in jpeg and sub_string in jpeg:
+        jpeg_path = os.path.join(jpeg_folder, jpeg)
+        im = np.array(Image.open(jpeg_path))
+        shape.append(im.shape)
+shape = np.array(shape)
+
+# Combine images after padding along rows and columns
+for i, jpeg in enumerate(jpeg_list):
+    if '.jpg' in jpeg and sub_string in jpeg:
+        jpeg_path = os.path.join(jpeg_folder, jpeg)
+        im = np.array(Image.open(jpeg_path))
+        x_width = np.max(shape[:, 0]) - im.shape[0]
+        y_width = np.max(shape[:, 1]) - im.shape[1]
+        im = np.pad(im, pad_width=((x_width//2,x_width-x_width//2), (y_width//2,y_width-y_width//2), (0,0)))
+
+        col_list.append(im)
+        img_name.append(jpeg)
+
+        if len(col_list) == nb_column:
+            line_list.append(np.concatenate(col_list, axis=1))
+            col_list = []
+        
+        if i == len(jpeg_list)-1:
+            last_row = np.concatenate(col_list, axis=1)
+            extra_pad = line_list[0].shape[1] - last_row.shape[1]
+            last_row = np.pad(last_row, pad_width=((0,0), (extra_pad//2,extra_pad-extra_pad//2), (0,0)))
+            line_list.append(last_row)
+
+out_img = np.concatenate(line_list, axis=0)
+cv2.imwrite(os.path.join(jpeg_folder,'res.png'), out_img)
