08fe6f448d9cb1466266f04d3ff5314d6638b96f
Nathan Molinier <nathan.molinier@gmail.com>
2024-10-19 17:41:31 -0400

    normalize img and deal with not git annexed

--- generate_middle_slices_BIDS.py
+++ generate_middle_slices_BIDS.py
@@ -4,12 +4,14 @@ import glob
 import cv2
 import numpy as np
 from progress.bar import Bar
+from vrac.utils.utils import normalize
 
-path_to_BIDS = '/Users/nathan/data/whole-spine'
+path_to_BIDS = '/Users/nathan/data/rootlets-ulrike/hc-leipzig-7t-mp2rage'
+contrast = ''
 output_folder_path = os.path.join(path_to_BIDS, 'derivatives/preview')
 
 # Fetch all the niftii files in the BIDS folder
-glob_files = glob.glob(os.path.join(path_to_BIDS,'**', '*.nii.gz'), recursive=True)
+glob_files = glob.glob(os.path.join(path_to_BIDS,'**', f'*{contrast}.nii.gz'), recursive=True)
 
 # Remove files from derivatives to only have images
 nii_files = [file for file in glob_files if 'derivatives' not in file]
@@ -22,9 +24,17 @@ if not os.path.exists(output_folder_path):
 bar = Bar('Convert data ', max=len(nii_files))
 
 for file_path in nii_files:
-    img = Image(file_path).change_orientation('RSP')
-    arr = np.array(img.data)
-    ind = arr.shape[0]//2
+    try:
+        img = Image(file_path).change_orientation('RSP')
+        arr = np.array(img.data)
+        ind = arr.shape[0]//2
 
-    file_name = os.path.basename(file_path).split('.')[0] + '.png'
-    cv2.imwrite(os.path.join(output_folder_path, file_name), arr[ind, :, :])
+        # Normalize
+        slice_img = arr[ind, :, :]
+        percentile = np.percentile(slice_img, 95)
+        slice_img_norm = normalize(np.clip(slice_img, None, percentile))
+
+        file_name = os.path.basename(file_path).split('.')[0] + '.png'
+        cv2.imwrite(os.path.join(output_folder_path, file_name), slice_img_norm*255)
+    except:
+        pass
