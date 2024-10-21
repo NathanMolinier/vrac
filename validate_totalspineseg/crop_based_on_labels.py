457d75ce6d90c5aec4b92297d70a89f90128d662
Nathan Molinier <nathan.molinier@gmail.com>
2024-10-21 07:51:43 -0400

    make script more robust

--- validate_totalspineseg/crop_based_on_labels.py
+++ validate_totalspineseg/crop_based_on_labels.py
@@ -3,22 +3,30 @@ from vrac.data_management.image import Image
 import numpy as np
 import subprocess
 
-img_folder = '/Users/nathan/Desktop/input'
-label_folder = '/Users/nathan/Desktop/step2_output'
-crop_folder = '/Users/nathan/Desktop/input_crop'
-exclude_labels_top = [40, 41, 224] # Exclude  vertebrae C1, vertebrae C2 and disc C2-C3
-exclude_labels_bottom = [92, 202] # Exclude sacrum, disc L5-S1
+img_folder = '/home/GRAMES.POLYMTL.CA/p118739/data/datasets/article-totalspineseg/localizer-based/raw'
+label_folder = '/home/GRAMES.POLYMTL.CA/p118739/data/datasets/article-totalspineseg/localizer-based/out/step2_output'
+crop_folder = '/home/GRAMES.POLYMTL.CA/p118739/data/datasets/article-totalspineseg/localizer-based/raw_crop'
+exclude_labels_top = [11, 12, 63, 71, 13, 72] # Exclude  vertebrae C1, vertebrae C2, disc C2-C3 and disc C7-T1
+exclude_labels_bottom = [50, 100] # Exclude sacrum, disc L5-S1
 
-for label_name in os.listdir(label_folder):
+for label_name in os.listdir(img_folder):
     if label_name.endswith('.nii.gz'):
         label_path = os.path.join(label_folder, label_name)
-        img_path = os.path.join(img_folder, label_name.replace('.nii.gz', '_0000.nii.gz'))
-        out_path = os.path.join(crop_folder, label_name.replace('.nii.gz', '_0000_crop.nii.gz'))
-        label = Image(label_path)
+        img_path = os.path.join(img_folder, label_name)#.replace('.nii.gz', '_0000.nii.gz'))
+        out_path = os.path.join(crop_folder, label_name)
         img = Image(img_path)
-        shape = label.data.shape
+        if os.path.exists(label_path):
+            label = Image(label_path)
 
-        if label.orientation == 'LPI' and img.orientation == 'LPI':
+            # Keep original orientation
+            ori_img = img.orientation
+            ori_label = label.orientation
+
+            # Change orientation
+            img.change_orientation('LPI')
+            label.change_orientation('LPI')
+            shape = label.data.shape
+
             # Extract vertical coordinates z_min and z_max for cropping
             z_max = shape[2]-1
             for ex_label in exclude_labels_top:
@@ -38,14 +46,24 @@ for label_name in os.listdir(label_folder):
             if not os.path.exists(crop_folder):
                 os.makedirs(crop_folder)
             
+            # Save img in out folder
+            img.save(out_path)
+
             subprocess.run([
                 'sct_crop_image',
-                '-i', img_path,
-                '-zmin', str(z_min + 5),
-                '-zmax', str(z_max - 5),
+                '-i', out_path,
+                '-zmin', str(z_min),
+                '-zmax', str(z_max),
                 '-o', out_path
+            ])
+
+            subprocess.run([
+                'sct_image',
+                '-i', out_path,
+                '-setorient', ori_img
             ])
             print(f'Image {img_path} was cropped')
         else:
-            print(f'Label is {label.orientation} and image is {img.orientation}')
+            # Save img in out folder
+            img.save(out_path)
         
