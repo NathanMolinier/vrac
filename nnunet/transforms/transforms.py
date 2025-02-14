c2ede3ebecf3eba1c9544e9307a74704704c6050
Nathan Molinier <nathan.molinier@gmail.com>
2025-02-14 13:12:08 -0500

    add ConvTransform

--- nnunet/transforms/transforms.py
+++ nnunet/transforms/transforms.py
@@ -1,26 +1,111 @@
 import torch
+import torch.nn.functional as F
 
 from batchgeneratorsv2.helpers.scalar_type import RandomScalar, sample_scalar
 from batchgeneratorsv2.transforms.base.basic_transform import ImageOnlyTransform, BasicTransform
 from typing import Tuple
 
+import numpy as np
+import torchio as tio
+import gryds
+import scipy.ndimage as ndi
+from scipy.stats import norm
+
 ### Contrast transform (Laplace, Gamma, Histogram Equalization, Log, Sqrt, Exp, Sin, Sig, Inverse)
 
-class LaplaceTransform(ImageOnlyTransform):
-    def __init__(self, absolute: bool = False):
+class ConvTransform(ImageOnlyTransform):
+    '''
+    Based on https://github.com/spinalcordtoolbox/disc-labeling-playground/blob/main/src/ply/models/transform.py
+    '''
+    def __init__(self, kernel_type: str = 'Laplace', absolute: bool = False):
         super().__init__()
+        if kernel_type not in  ["Laplace","Scharr"]:
+            raise NotImplementedError('Currently only "Laplace" and "Scharr" are supported.')
+        else:
+            self.kernel_type = kernel_type
         self.absolute = absolute
 
     def get_parameters(self, **data_dict) -> dict:
+        spatial_dims = len(data_dict['image'].shape) - 1
+        if spatial_dims == 2:
+            if self.kernel_type == "Laplace":
+                kernel = torch.tensor([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=torch.float32)
+            elif self.kernel_type == "Scharr":
+                kernel_x = torch.tensor([[-3, 0, 3], [-10, 0, -10], [-3, 0, 3]], dtype=torch.float32)
+                kernel_y = torch.tensor([[-3, -10, -3], [0, 0, 0], [3, 10, 3]], dtype=torch.float32)
+                kernel = [kernel_x, kernel_y]
+        elif spatial_dims == 3:
+            if self.kernel_type == "Laplace":
+                kernel = -1.0 * torch.ones(3, 3, 3, dtype=torch.float32)
+                kernel[1, 1, 1] = 26.0
+            elif self.kernel_type == "Scharr":
+                kernel_x = torch.tensor([[[  9,    0,    -9],
+                                          [ 30,    0,   -30],
+                                          [  9,    0,    -9]],
+
+                                          [[ 30,    0,   -30],
+                                           [100,    0,  -100],
+                                           [ 30,    0,   -30]],
+
+                                          [[  9,    0,    -9],
+                                           [ 30,    0,   -30],
+                                           [  9,    0,    -9]]], dtype=torch.float32)
+                
+                kernel_y = torch.tensor([[[    9,   30,    9],
+                                          [    0,    0,    0],
+                                          [   -9,  -30,   -9]],
+
+                                         [[  30,  100,   30],
+                                          [   0,    0,    0],
+                                          [ -30, -100,  -30]],
+
+                                         [[   9,   30,    9],
+                                          [   0,    0,    0],
+                                          [  -9,  -30,   -9]]], dtype=torch.float32)
+                
+                kernel_z = torch.tensor([[[   9,   30,   9],
+                                          [  30,  100,  30],
+                                          [   9,   30,   9]],
+
+                                         [[   0,    0,   0],
+                                          [   0,    0,   0],
+                                          [   0,    0,   0]],
+
+                                         [[   -9,  -30,  -9],
+                                          [  -30, -100, -30],
+                                          [   -9,  -30,  -9]]], dtype=torch.float32)
+                kernel = [kernel_x, kernel_y, kernel_z]
+        else:
+            raise ValueError(f"{self.__class__} can only handle 2D or 3D images.")
+
         return {
+            'kernel_type': self.kernel_type,
+            'kernel': kernel,
             'absolute': self.absolute
         }
     
     def _apply_to_image(self, img: torch.Tensor, **params) -> torch.Tensor:
-        if len(params['axes']) == 0:
-            return img
-        axes = [i + 1 for i in params['axes']]
-        return torch.flip(img, axes)
+        '''
+        We expect (C, X, Y) or (C, X, Y, Z) shaped inputs for image and seg
+        '''
+        for c in img.shape[0]:
+            orig_mean = torch.mean(img[c])
+            orig_std = torch.std(img[c])
+            img_ = img[c].unsqueeze(0)  # adds a batch dim
+            if params['kernel_type'] == 'Laplace':
+                tot_ = apply_filter(img_, params['kernel'])
+            elif params['kernel_type'] == 'Scharr':
+                tot_ = torch.zeros_like(img_)
+                for kernel in params['kernel']:
+                    if params['absolute']:
+                        tot_ += torch.abs(apply_filter(img_, kernel))
+                    else:
+                        tot_ += apply_filter(img_, kernel)
+            mean = torch.mean(tot_)
+            std = torch.std(tot_)
+            img[c] = (tot_ - mean)/torch.clamp(std, min=1e-7)
+            img[c] = img[c]*orig_std + orig_mean # return to original distribution
+        return img
 
 
 class LaplaceTransform(ImageOnlyTransform):
@@ -155,21 +240,54 @@ class MirrorTransform(BasicTransform):
 
 ### Utils function
 
-def aug_transform(img, transform):
+def apply_filter(x: torch.Tensor, kernel: torch.Tensor, **kwargs) -> torch.Tensor:
     """
-    Augment the image by applying a given transformation function.
-    Based on https://github.com/neuropoly/totalspineseg/blob/main/totalspineseg/utils/augment.py
+    Codpied from https://github.com/Project-MONAI/MONAI/blob/dev/monai/networks/layers/simplelayers.py
+
+    Filtering `x` with `kernel` independently for each batch and channel respectively.
+
+    Args:
+        x: the input image, must have shape (batch, channels, H[, W, D]).
+        kernel: `kernel` must at least have the spatial shape (H_k[, W_k, D_k]).
+            `kernel` shape must be broadcastable to the `batch` and `channels` dimensions of `x`.
+        kwargs: keyword arguments passed to `conv*d()` functions.
+
+    Returns:
+        The filtered `x`.
+
+    Examples:
+
+    .. code-block:: python
+
+        >>> import torch
+        >>> from monai.networks.layers import apply_filter
+        >>> img = torch.rand(2, 5, 10, 10)  # batch_size 2, channels 5, 10x10 2D images
+        >>> out = apply_filter(img, torch.rand(3, 3))   # spatial kernel
+        >>> out = apply_filter(img, torch.rand(5, 3, 3))  # channel-wise kernels
+        >>> out = apply_filter(img, torch.rand(2, 5, 3, 3))  # batch-, channel-wise kernels
+
     """
-    # Compute original mean, std and min/max values
-    img_min, img_max = img.min(), img.max()
-    # Normlize
-    img = (img - img.mean()) / img.std()
-    img = np.interp(img, (img.min(), img.max()), (0, 1))
+    if not isinstance(x, torch.Tensor):
+        raise TypeError(f"x must be a torch.Tensor but is {type(x).__name__}.")
+    batch, chns, *spatials = x.shape
+    n_spatial = len(spatials)
+    if n_spatial > 3:
+        raise NotImplementedError(f"Only spatial dimensions up to 3 are supported but got {n_spatial}.")
+    k_size = len(kernel.shape)
+    if k_size < n_spatial or k_size > n_spatial + 2:
+        raise ValueError(
+            f"kernel must have {n_spatial} ~ {n_spatial + 2} dimensions to match the input shape {x.shape}."
+        )
+    kernel = kernel.to(x)
+    # broadcast kernel size to (batch chns, spatial_kernel_size)
+    kernel = kernel.expand(batch, chns, *kernel.shape[(k_size - n_spatial) :])
+    kernel = kernel.reshape(-1, 1, *kernel.shape[2:])  # group=1
+    x = x.view(1, kernel.shape[0], *spatials)
+    conv = [F.conv1d, F.conv2d, F.conv3d][n_spatial - 1]
+    if "padding" not in kwargs:
+        kwargs["padding"] = "same"
 
-    # Transform
-    img = transform(img)
-
-    # Return to original range
-    img = np.interp(img, (img.min(), img.max()), (img_min, img_max))
-
-    return img
+    if "stride" not in kwargs:
+        kwargs["stride"] = 1
+    output = conv(x, kernel, groups=kernel.shape[0], bias=None, **kwargs)
+    return output.view(batch, chns, *output.shape[2:])
