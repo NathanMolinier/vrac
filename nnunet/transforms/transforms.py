3c75216658f2c1bd78ecc6863d6dedc1aa511cd3
Nathan Molinier <nathan.molinier@gmail.com>
2025-02-21 16:51:33 -0500

    add anisotropy function

--- nnunet/transforms/transforms.py
+++ nnunet/transforms/transforms.py
@@ -477,95 +477,12 @@ def aug_elastic(img, seg):
     ))
     return subject.image.data, subject.seg.data
 
-
-### Anisotropy augmentation
-
-
-class GammaTransform(ImageOnlyTransform):
-    def __init__(self, gamma: RandomScalar, p_invert_image: float, synchronize_channels: bool, p_per_channel: float,
-                 p_retain_stats: float):
-        super().__init__()
-        self.gamma = gamma
-        self.p_invert_image = p_invert_image
-        self.synchronize_channels = synchronize_channels
-        self.p_per_channel = p_per_channel
-        self.p_retain_stats = p_retain_stats
-
-    def get_parameters(self, **data_dict) -> dict:
-        shape = data_dict['image'].shape
-        apply_to_channel = torch.where(torch.rand(shape[0]) < self.p_per_channel)[0]
-        retain_stats = torch.rand(len(apply_to_channel)) < self.p_retain_stats
-        invert_image = torch.rand(len(apply_to_channel)) < self.p_invert_image
-
-        if self.synchronize_channels:
-            gamma = torch.Tensor([sample_scalar(self.gamma, image=data_dict['image'], channel=None)] * len(apply_to_channel))
-        else:
-            gamma = torch.Tensor([sample_scalar(self.gamma, image=data_dict['image'], channel=c) for c in apply_to_channel])
-        return {
-            'apply_to_channel': apply_to_channel,
-            'retain_stats': retain_stats,
-            'invert_image': invert_image,
-            'gamma': gamma
-        }
-
-    def _apply_to_image(self, img: torch.Tensor, **params) -> torch.Tensor:
-        for c, r, i, g in zip(params['apply_to_channel'], params['retain_stats'], params['invert_image'], params['gamma']):
-            if i:
-                img[c] *= -1
-            if r:
-                # std_mean is for whatever reason slower than doing the computations separately!?
-                # std, mean = torch.std_mean(img[c])
-                mean = torch.mean(img[c])
-                std = torch.std(img[c])
-            minm = torch.min(img[c])
-            rnge = torch.max(img[c]) - minm
-            img[c] = torch.pow(((img[c] - minm) / torch.clamp(rnge, min=1e-7)), g) * rnge + minm
-            if r:
-                # std_here, mn_here = torch.std_mean(img[c])
-                mn_here = torch.mean(img[c])
-                std_here = torch.std(img[c])
-                img[c] -= mn_here
-                img[c] *= (std / torch.clamp(std_here, min=1e-7))
-                img[c] += mean
-
-            if i:
-                img[c] *= -1
-        return img
-
-class MirrorTransform(BasicTransform):
-    def __init__(self, allowed_axes: Tuple[int, ...]):
-        super().__init__()
-        self.allowed_axes = allowed_axes
-
-    def get_parameters(self, **data_dict) -> dict:
-        axes = [i for i in self.allowed_axes if torch.rand(1) < 0.5]
-        return {
-            'axes': axes
-        }
-
-    def _apply_to_image(self, img: torch.Tensor, **params) -> torch.Tensor:
-        if len(params['axes']) == 0:
-            return img
-        axes = [i + 1 for i in params['axes']]
-        return torch.flip(img, axes)
-
-    def _apply_to_segmentation(self, segmentation: torch.Tensor, **params) -> torch.Tensor:
-        if len(params['axes']) == 0:
-            return segmentation
-        axes = [i + 1 for i in params['axes']]
-        return torch.flip(segmentation, axes)
-
-    def _apply_to_regr_target(self, regression_target, **params) -> torch.Tensor:
-        if len(params['axes']) == 0:
-            return regression_target
-        axes = [i + 1 for i in params['axes']]
-        return torch.flip(regression_target, axes)
-
-    def _apply_to_bbox(self, bbox, **params):
-        raise NotImplementedError
-
-    def _apply_to_keypoints(self, keypoints, **params):
-        raise NotImplementedError
+def aug_anisotropy(img, seg, downsampling=7):
+    subject = tio.RandomAnisotropy(downsampling=downsampling)(tio.Subject(
+        image=tio.ScalarImage(tensor=img),
+        seg=tio.LabelMap(tensor=seg, axis=0)
+    ))
+    return subject.image.data, subject.seg.data
 
 
 ### Utils function
