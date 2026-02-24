e8dbd3c3d8fe6f7c5e2e7f6738544069b234eb1c
Nathan Molinier <nathan.molinier@gmail.com>
2026-02-24 16:10:00 -0500

    use median value for eccentricity, solidity and orientation

--- spinereports/analysis_balgrist.py
+++ spinereports/analysis_balgrist.py
@@ -124,19 +124,33 @@ def _flatten_grouped_stats_canal(
 			for col in numeric_cols:
 				key = f"{prefix}_{disc_level}_{_safe_col(col)}_ratio"
 				val_list = [df_ordered[col].to_numpy()[j] for j in range(i-2, i+2)]
-				prev_vertebra_key = f"{prefix}_{_safe_col(col)}_max_{_safe_col(str(levels[i-1]))}"
-				curr_vertebra_key = f"{prefix}_{_safe_col(col)}_max_{_safe_col(str(levels[i]))}"
+				if col in ['eccentricity', 'solidity', 'orientation']:
+					stat = 'median'
+				else:
+					stat = 'max'
+				prev_vertebra_key = f"{prefix}_{_safe_col(col)}_{stat}_{_safe_col(str(levels[i-1]))}"
+				curr_vertebra_key = f"{prefix}_{_safe_col(col)}_{stat}_{_safe_col(str(levels[i]))}"
 				if curr_vertebra_key in vertebra_avg and prev_vertebra_key in vertebra_avg:
 					denom = (vertebra_avg[curr_vertebra_key] + vertebra_avg[prev_vertebra_key])
-					out[key] = 2*np.min(val_list)/denom if denom != 0 else np.nan
+					if col in ['eccentricity', 'solidity', 'orientation']:
+						out[key] = 2*np.median(val_list)/denom if denom != 0 else np.nan
+					else:
+						out[key] = 2*np.min(val_list)/denom if denom != 0 else np.nan
 	
 	# Approximate L5-S1 level
 	if levels[0] == "L5":
 		for col in numeric_cols:
 			key = f"{prefix}_L5-S1_{_safe_col(col)}_ratio"
 			val_list = df_ordered[col].to_numpy()[0:3]
-			denom = vertebra_avg[f"{prefix}_{_safe_col(col)}_max_L5"]
-			out[key] = np.min(val_list)/denom if denom != 0 else np.nan 
+			if col in ['eccentricity', 'solidity', 'orientation']:
+				stat = 'median'
+			else:
+				stat = 'max'
+			denom = vertebra_avg[f"{prefix}_{_safe_col(col)}_{stat}_L5"]
+			if col in ['eccentricity', 'solidity', 'orientation']:
+				out[key] = np.median(val_list)/denom if denom != 0 else np.nan
+			else:
+				out[key] = np.min(val_list)/denom if denom != 0 else np.nan 
 
 	return out
 
@@ -261,7 +275,7 @@ def load_subject_features(reports_dir: Path) -> "pd.DataFrame":
 						canal,
 						prefix="canal",
 						group_col="vertebra_level",
-						stats=("mean", "std", "median", "max"),
+						stats=("mean", "std", "median", "max", "min"),
 						drop_cols=("slice_interp", "structure", "structure_name"),
 					)
 				)
