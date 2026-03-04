2f9e1f23231ba1d741ff3f406127ef1bb181b2fc
Nathan Molinier <nathan.molinier@gmail.com>
2026-03-04 11:32:35 -0500

    update file naming

--- split_nifti.py
+++ split_nifti.py
@@ -3,8 +3,8 @@ import os
 
 
 def main():
-    img_4d_path = "/Users/nathan/data/fast-headPos/_localizer_fast_FA3_TR10_BW240_20meas_20250604135044_28.nii"
-    out_folder = "/Users/nathan/data/fast-headPos/split/"
+    img_4d_path = "/home/GRAMES.POLYMTL.CA/p118739/data_nvme_p118739/data/datasets/fast-mri/3D/niftii/08-localizer_fast_FA3_TR10_BW240_5slc_15meas_p2_1.5mm_localizer_fast_FA3_TR10_BW240_5slc_15meas_p2_1.5mm_20260120091318_8.nii.gz"
+    out_folder = "/home/GRAMES.POLYMTL.CA/p118739/data_nvme_p118739/data/datasets/fast-mri/3D/niftii/split_08/"
 
     if not os.path.exists(out_folder):
         os.makedirs(out_folder)
@@ -12,9 +12,10 @@ def main():
     img_4d = Image(img_4d_path).change_orientation('RPI')
 
     data = img_4d.data
+    img_name = os.path.basename(img_4d_path).split('.')[0]
 
     for i in range(data.shape[-1]):
-        out_path = os.path.join(out_folder, f"localizer_fast_FA3_TR10_BW240_meas{i+1:03}.nii.gz")
+        out_path = os.path.join(out_folder, f"{img_name}_meas{i+1:03}.nii.gz")
         Image(data[:,:,:,i], hdr=img_4d.hdr).save(out_path)
 
 if __name__=='__main__':
