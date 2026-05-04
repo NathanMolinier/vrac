2c87c334b78d4652bc5a872e0fe273f1b6996384
Nathan Molinier <nathan.molinier@polymtl.ca>
2026-05-04 11:28:36 -0400

    allow json input

--- spinereports/fetch_data_info.py
+++ spinereports/fetch_data_info.py
@@ -4,15 +4,15 @@ import json
 import nibabel as nib
 import pandas as pd
 from pathlib import Path
-from src.vrac.data_management.image import Image
+from vrac.data_management.image import Image
 
 def get_parser():
     """
     Creates an argument parser for the script.
     """
     parser = argparse.ArgumentParser(description='Fetch information from BIDS dataset JSON sidecars.')
-    parser.add_argument('--path-dataset', required=True, help='Path to the BIDS dataset.')
-    parser.add_argument('--file-pattern', required=True, help='Pattern to match files (e.g., "*_acq-sag_T2w*").')
+    parser.add_argument('--path-input', required=True, help='If path endswith .json, will be treated as config else treated as an input folder path to a BIDS dataset.')
+    parser.add_argument('--file-pattern', default='*', help='Pattern to match files (e.g., "*_acq-sag_T2w*").')
     parser.add_argument('--output-csv', help='Path to save the output CSV file.')
     return parser
 
@@ -23,11 +23,18 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    dataset_path = Path(args.path_dataset)
-    file_pattern = args.file_pattern
+    dataset_path = Path(args.path_input)
+    if dataset_path.suffix == '.json':
+        with open(dataset_path, 'r') as f:
+            config = json.load(f)
+        split = 'TRAINING'
+        dataset_path = Path(config['DATASETS_PATH'])
+        image_files = [dataset_path /d['IMAGE'] for d in config[split]]
+    else:
+        file_pattern = args.file_pattern
+        image_files = sorted(list(dataset_path.glob(f'**/{file_pattern}.nii.gz')))
+    
     output_csv = args.output_csv
-
-    image_files = sorted(list(dataset_path.glob(f'**/{file_pattern}.nii.gz')))
     
     data_list = []
 
