6781715d744a39056f6ee0966a92249dd7c1be04
Nathan Molinier <nathan.molinier@polymtl.ca>
2026-07-28 13:11:12 -0400

    update

--- validate_totalspineseg/match_label_spineps.py
+++ validate_totalspineseg/match_label_spineps.py
@@ -1,20 +1,23 @@
-from vrac.data_management.image import Image
+from vrac.data_management.image import Image, zeros_like
 import os
 import numpy as np
 
 def main():
     # Set paths
-    spineps_folder = '/home/ge.polymtl.ca/p118739/data/datasets/article-totalspineseg/spider-comparison/spineps'
-    ground_truth_folder = '/home/ge.polymtl.ca/p118739/data/datasets/article-totalspineseg/spider-comparison/gt'
+    spineps_folder = '/home/ge.polymtl.ca/p118739/data/datasets/article-totalspineseg/totalsegXtotalspinesegXspineps/spineps/vert-disc/spineps-pred'
+    ground_truth_folder = '/home/ge.polymtl.ca/p118739/data/datasets/article-totalspineseg/totalsegXtotalspinesegXspineps/spineps/vert-disc/gt-spider'
+    output_folder = '/home/ge.polymtl.ca/p118739/data/datasets/article-totalspineseg/totalsegXtotalspinesegXspineps/spineps/vert-disc/spineps-pred-newlabel'
 
     for file in os.listdir(spineps_folder):
         if file.endswith(".nii.gz"):
             spineps_path = os.path.join(spineps_folder, file)
             gt_path = os.path.join(ground_truth_folder, file)
+            output_path = os.path.join(output_folder, file)
 
             # Load images
             spineps_img = Image(spineps_path)
             gt_img = Image(gt_path)
+            out_img = zeros_like(gt_img)
 
             # Compare labels
             gt_labels = set(np.unique(gt_img.data))
@@ -23,7 +26,10 @@ def main():
                 mask = (gt_img.data == label)
                 spineps_values = spineps_img.data[mask]
 
-                print()
+                print(label)
+                print(spineps_values)
+                break
+            break
 
                 
             print(f"File: {file}")
