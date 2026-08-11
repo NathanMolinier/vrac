72b91a7ee4b957f1f4c2465e06fcc91d0ee3143c
Nathan Molinier <nathan.molinier@gmail.com>
2026-08-11 16:49:41 -0400

    fix code to account for existing subjects in dataset

--- BIDSIFICATION/lumbar-usf/add_new_data.py
+++ BIDSIFICATION/lumbar-usf/add_new_data.py
@@ -35,31 +35,7 @@ def create_subject_folder_if_not_exists(path_subject_folder_out):
         logger.info(f'Creating directory: {path_subject_folder_out}')
 
 
-def copy(path_file_in, path_file_out):
-    """
-    Copy file from the input dataset to the output dataset
-    :param path_file_in: path to file in the input dataset
-    :param path_file_out: path to file in the output dataset
-    """
-    shutil.copy(path_file_in, path_file_out)
-    logger.info(f'Copying: {path_file_in} --> {path_file_out}')
-
-
-def write_json(path_output, json_filename, data_json):
-    """
-    :param path_output: path to the output BIDS folder
-    :param json_filename: json filename, for example: participants.json
-    :param data_json: JSON formatted content
-    :return:
-    """
-    with open(os.path.join(path_output, json_filename), 'w') as json_file:
-        json.dump(data_json, json_file, indent=4)
-        # Add last newline
-        json_file.write("\n")
-        logger.info(f'{json_filename} created in {path_output}')
-        
-
-def create_participants_tsv(participants_tsv_list, path_output):
+def edit_participants_tsv(participants_tsv_list, path_output):
     """
     Write participants.tsv file
     :param participants_tsv_list: list containing [subject_out, pathology_out, subject_in, centre_in, centre_out],
@@ -67,6 +43,11 @@ def create_participants_tsv(participants_tsv_list, path_output):
     :param path_output: path to the output BIDS folder
     :return:
     """
+    if os.path.exists(os.path.join(path_output, 'participants.tsv')):
+        with open(os.path.join(path_output, 'participants.tsv'), 'r') as tsv_file:
+            tsv_reader = csv.reader(tsv_file, delimiter='\t')
+            list_subjects = [row for row in tsv_reader][1:]  # Skip header
+    
     with open(os.path.join(path_output, 'participants.tsv'), 'w') as tsv_file:
         tsv_writer = csv.writer(tsv_file, delimiter='\t', lineterminator='\n')
         tsv_writer.writerow(['participant_id', 'source_id', 'species', 'age', 'sex', 'pathology', 'institution', 'notes'])
@@ -77,10 +58,14 @@ def create_participants_tsv(participants_tsv_list, path_output):
         # Extra info
         extra_data = ['n/a', 'n/a', 'n/a', 'n/a', 'n/a']
 
+        # Add new subjects
+        for participant in participants_tsv_list:
+            list_subjects.append([participant[0], participant[1]] + species + extra_data)
+
         # Add rows to tsv file
-        participants_tsv_list = sorted(participants_tsv_list, key=lambda a : a[0])
-        for item in participants_tsv_list:
-            tsv_writer.writerow(list(item) + species + extra_data)
+        list_subjects = sorted(list_subjects, key=lambda a : a[0])
+        for item in list_subjects:
+            tsv_writer.writerow(item)
         logger.info(f'participants.tsv created in {path_output}')
 
 
@@ -128,35 +113,36 @@ def main():
     # Initialize dict for participants.tsv
     sub_dict_tsv = dict()
     for file in os.listdir(path_dataset):
-        path_input = os.path.join(path_dataset, file)
-        old_filename = os.path.basename(path_input)
-        old_name = old_filename.split('_')[0]
-        bids_filename = old_filename.replace('RESTORE_','')
-        session = bids_filename.split('_ses-')[1].split('_')[0]
-        subject_name_bids = bids_filename.split('_')[0]
-        
-        # Add subject name to participant.tsv 
-        if subject_name_bids not in sub_dict_tsv.keys(): # Add only one time each subject into the participant.csv
-            # Aggregate subjects for participants.tsv
-            sub_dict_tsv[subject_name_bids] = old_name
+        if file.endswith('.nii.gz'):
+            path_input = os.path.join(path_dataset, file)
+            old_filename = os.path.basename(path_input)
+            old_name = "_".join(old_filename.split('_')[0:2])
+            bids_filename = old_filename.replace('RESTORE_','')
+            session = bids_filename.split('_ses-')[1].split('_')[0]
+            subject_name_bids = bids_filename.split('_')[0]
+            
+            # Add subject name to participant.tsv 
+            if subject_name_bids not in sub_dict_tsv.keys(): # Add only one time each subject into the participant.csv
+                # Aggregate subjects for participants.tsv
+                sub_dict_tsv[subject_name_bids] = old_name
 
-        # Path input image
-        path_file_in = os.path.join(path_input)
-        
-        # Load image and reorient
-        img = Image(path_file_in).change_orientation('RPI')
-        
-        # Construct path for the output IMAGE
-        path_subject_folder_out = os.path.join(path_output, subject_name_bids, f'ses-{session}', 'anat')
-        create_subject_folder_if_not_exists(path_subject_folder_out)
+            # Path input image
+            path_file_in = os.path.join(path_input)
+            
+            # Load image and reorient
+            img = Image(path_file_in).change_orientation('RPI')
+            
+            # Construct path for the output IMAGE
+            path_subject_folder_out = os.path.join(path_output, subject_name_bids, f'ses-{session}', 'anat')
+            create_subject_folder_if_not_exists(path_subject_folder_out)
 
-        # Save file nifti
-        path_file_out = os.path.join(path_subject_folder_out, bids_filename)
-        img.save(path_file_out)
+            # Save file nifti
+            path_file_out = os.path.join(path_subject_folder_out, bids_filename)
+            img.save(path_file_out)
     
     participants_tsv_list = list(sub_dict_tsv.items())                 
 
-    create_participants_tsv(participants_tsv_list, path_output)
+    edit_participants_tsv(participants_tsv_list, path_output)
     copy_script(path_output)
 
 if __name__ == "__main__":
