2a5f8a88aefb6b50118b03a5d64c8ab87618c1ca
Nathan Molinier <nathan.molinier@gmail.com>
2026-08-31 10:58:31 -0400

    add lasso

--- nnrefine.py
+++ nnrefine.py
@@ -89,7 +89,7 @@ def refine_segmentation(session, img_path, seg_path, output_path, exclude_labels
         if not l in exclude_labels:
             print(f"Refining segmentation for label {l}...")
             # Set target buffer to zero
-            results = refine_segmentation_single(session, seg_data, l, 0, 1, lasso=False)
+            results = refine_segmentation_single(session, seg_data, l, 0, 1, lasso=True)
         else:
             results = (seg_data == l).astype(np.int8)
         if results is not None:
